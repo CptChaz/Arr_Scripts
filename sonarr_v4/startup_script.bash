969a3fe251884a2e510e46be04bb536a5e719966
Cpt. Chaz <55520028+CptChaz@users.noreply.github.com>
2023-04-23 14:16:33 -0500

    Update startup_script.bash

--- sonarr_v4/startup_script.bash
+++ sonarr_v4/startup_script.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
-
-#This is intended to be a startup script for sonarr v4+. Currently the script installs python3, pip, and the requests module. 
-#Place script in the /custom-cont-init.d folder for execution at startup, and modify to fit individual need
-
+#
+# Author: [Cpt. Chaz]
+# Created: [04/23/23]
+# Description: This is intended to be a startup script for Sonarr v4+. Currently the script installs python3, pip, and the "requests" module. 
+# Place script in the /custom-cont-init.d folder for execution at startup, and modify to fit individual need
+#
+# Credits:
+# - This script was created with the help of ChatGPT, an OpenAI language model.
+#
 # Update the package manager and install Python 3, pip, and other dependencies
 apk update
 apk add python3 py3-pip libxml2 libxslt-dev libffi-dev gcc musl-dev openssl-dev
@@ -13,3 +18,5 @@ pip3 install requests
 echo " "
 echo "all done"
 echo " "
+
+

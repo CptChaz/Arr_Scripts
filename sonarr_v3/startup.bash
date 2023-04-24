d9586f16b768348b657fe1085018cd71a064da24
Cpt. Chaz <55520028+CptChaz@users.noreply.github.com>
2023-04-24 07:12:26 -0500

    Update and rename dependancies.bash to startup.bash

---
+++ sonarr_v3/startup.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Author: [Cpt. Chaz]
+# Created: [04/23/23]
+# Description: This is intended to be a startup script for Sonarr v3+. Currently the script installs python3, pip, and the "requests" module. 
+# Place script in the /custom-cont-init.d folder for execution at startup, and modify to fit individual need
+#
+# Credits:
+# - This script was created with the help of ChatGPT, an OpenAI language model.
+#
+#Update and upgrade before installing any packages
+echo "*** update & upgrade ***"
+yes Y | apt-get update && apt-get upgrade
+
+#Install Python3 & Pip
+echo "*** installing python3 ***"
+yes Y | apt-get install python3-pip
+
+sleep 3
+
+#Install Requests Module
+echo "*** installing requests python package ***"
+yes Y | python3 -m pip install requests
+
+echo "all done!"

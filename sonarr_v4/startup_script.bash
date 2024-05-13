d3e5549d99a5fa8f35b7c3c4ecb9497b5da4a5ef
Cpt. Chaz <55520028+CptChaz@users.noreply.github.com>
2024-05-13 14:42:46 -0500

    Update startup_script.bash
    update handling of installing requests module

--- sonarr_v4/startup_script.bash
+++ sonarr_v4/startup_script.bash
@@ -2,6 +2,7 @@
 #
 # Author: [Cpt. Chaz]
 # Created: [04/23/23]
+# Updated: [05/13/24
 # Description: This is intended to be a startup script for Sonarr v4+. Currently the script installs python3, pip, and the "requests" module. 
 # Place script in the /custom-cont-init.d folder for execution at startup, and modify to fit individual need
 # Status: Tested
@@ -11,10 +12,8 @@
 #
 # Update the package manager and install Python 3, pip, and other dependencies
 apk update
-apk add python3 py3-pip libxml2 libxslt-dev libffi-dev gcc musl-dev openssl-dev
+apk add python3 py3-pip libxml2 libxslt-dev libffi-dev gcc musl-dev openssl-dev py3-requests
 
-# Install the "requests" module
-pip3 install requests
 
 echo " "
 echo "all done"
